6e9f6857314afdd88792b280826bb7df7ab6205f
John Webster <45958589+Jwebster89@users.noreply.github.com>
2022-01-20 15:47:18 +1100

    Update Core_gene_phylogeny_runner.sh

--- Core_gene_phylogeny_runner.sh
+++ Core_gene_phylogeny_runner.sh
@@ -7,7 +7,7 @@ threads=$3
 panaroo -i $gff_dir/* -o $outdir -t $threads --clean-mode moderate -a core --core_threshold 0.9999
 cd $outdir
 modeltest_roary_core_gene_alignment.py core_gene_alignment.aln core_alignment_header.embl $threads core_gene_alignment
-XMFA_create.py core_gene_alignment.partitions.txt $outdir/aligned_gene_sequences
+XMFA_create.py core_gene_alignment.partitions.txt $outdir/.tmp
 iqtree -s core_gene_alignment.aln -p core_gene_alignment.partitions.txt -m GTR+I+F+R4 -T $threads
 ClonalFrameML core_gene_alignment.partitions.txt.treefile core_aln.xmfa core_gene_alignment.partitions.cfml.em -em true -emsim 100 -xmfa_file true
 maskrc-svg.py --aln core_gene_alignment.aln --out core_gene_alignment.rc_masked.aln --svg core_gene_alignment.rc_masked.svg core_gene_alignment.partitions.cfml.em
